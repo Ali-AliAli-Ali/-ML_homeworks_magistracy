f9f8a45cf6167facd58046005fec88bb50a06555
Ali-AliAli-Ali <alivolf@icloud.com>
2024-12-03 20:35:02 +0300

    Beautified 6,7.py

--- 6,7.py
+++ 6,7.py
@@ -4,7 +4,7 @@ pic = cv2.imread('meme.jpg')
 pic_flip_h = cv2.flip(pic, 1)  # 6
 pic_flip_v = cv2.flip(pic, 0)  # 7
 
-cv2.imshow('Original (color) picture', pic)
+cv2.imshow('Original picture', pic)
 cv2.imshow('Picture flipped horizontally', pic_flip_h)
 cv2.imshow('Picture flipped vertically', pic_flip_v)
 

7c519aeba2222e7c2038d823bbba26aa0db1eb7f
Ali-AliAli-Ali <alivolf@icloud.com>
2024-12-03 20:35:22 +0300

    Beautified 5.py

--- 5.py
+++ 5.py
@@ -3,7 +3,7 @@ import cv2
 pic = cv2.imread('meme.jpg')
 pic_hsv = cv2.cvtColor(pic, cv2.COLOR_BGR2HSV)
 
-cv2.imshow('Original (color) picture', pic)
+cv2.imshow('Original picture', pic)
 cv2.imshow ('Picture in hsv format', pic_hsv)
 
 cv2.waitKey(0)
